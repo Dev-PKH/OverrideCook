a6ddc37678505f8ed6aba0dd8f0443449c592292
Dev-PKH <tapasse12@gmail.com>
2025-07-20 22:26:05 +0900

    feat: 일시 정지 기능 구현
    - 일시 정지 기능 구현(GamePauseUI)
    - 이벤트 초기화(ResetStaticDataManager.cs)

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -7,18 +7,39 @@ using UnityEngine;
 // 게임 입력 처리(상호작용 등)
 public class GameInput : MonoBehaviour
 {
+    public static GameInput Instance { get; private set; }
+
     public event EventHandler OnInteractAction; // 상호작용 이벤트 핸들러
     public event EventHandler OnInteractAlternateAction; // 슬라이스 상호작용 이벤트 핸들러
+    public event EventHandler OnPuaseAction;
 
     private PlayerInputActions playerInputActions; // InputSystem
 
     private void Awake()
     {
+        if(Instance == null) Instance = this;
+
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed; // 상호작용 키 입력 시 실행할 이벤트
         playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
+        playerInputActions.Player.Pause.performed += Pause_performed;
+    }
+
+    private void OnDestroy()
+    {
+        // 이벤트 해제를 안하면 다시 나갔다 esc를 누를 때 Missing 에러가  발생함
+        playerInputActions.Player.Interact.performed -= Interact_performed; // 상호작용 키 입력 시 실행할 이벤트
+        playerInputActions.Player.InteractAlternate.performed -= InteractAlternate_performed;
+        playerInputActions.Player.Pause.performed -= Pause_performed;
+
+        playerInputActions.Dispose();
+    }
+
+    private void Pause_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnPuaseAction?.Invoke(this, EventArgs.Empty);
     }
 
     private void InteractAlternate_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -8,6 +8,8 @@ public class KitchenGameManager : MonoBehaviour
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
     private enum State
     { 
         WaitingToStart,
@@ -21,12 +23,23 @@ public class KitchenGameManager : MonoBehaviour
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
     private float gamePlayingTimerMax = 10f;
+    private bool isGamePaused = false;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
 
         state = State.WaitingToStart;
+    }
+
+    private void Start()
+    {
+        GameInput.Instance.OnPuaseAction += GameInput_OnPuaseAction;
+    }
+
+    private void GameInput_OnPuaseAction(object sender, EventArgs e)
+    {
+        TogglePuaseGame();
     }
 
     private void Update()
@@ -88,5 +101,20 @@ public class KitchenGameManager : MonoBehaviour
     public float GetGamePlayingTimerNormalized()
     {
         return 1- (gamePlayingTimer / gamePlayingTimerMax);
+    }
+
+    public void TogglePuaseGame()
+    {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
