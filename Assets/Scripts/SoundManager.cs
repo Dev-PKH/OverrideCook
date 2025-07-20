9cf591ce4ae612e93c8825a2184e4048ee14cb7f
Dev-PKH <tapasse12@gmail.com>
2025-07-20 23:09:13 +0900

    feat: 사운드 설정 구현
    - 사운드 설정 기능 구현(SoundManager.cs)
    
    > 부동 소수점 문제 발생
    > esc 종료시 옵션창 그대로 유지됨

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,11 +6,17 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+    
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private float volume = 1f;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
     private void Start()
@@ -63,13 +69,30 @@ public class SoundManager : MonoBehaviour
         PlaySound(audiopClipArray[Random.Range(0, audiopClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audiopClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audiopClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audiopClip, position, volume);
+        AudioSource.PlayClipAtPoint(audiopClip, position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if(volume >1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
     }
 }
