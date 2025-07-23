67a98f71b598a4027b72dbf53993dd1b6491ff04
Dev-PKH <tapasse12@gmail.com>
2025-07-23 20:57:24 +0900

    feat: 픽업 동기화
    - KitchenObject를 가지고 다닐 때 위치를 동기화(FollowTransfomr.cs)
    - 카운터 애니메이션 동기화(Container.Countercs)

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -9,6 +9,12 @@ public class KitchenObject : NetworkBehaviour
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     private IKitchenObjectParent kitchenObjectParent; // 현재 위치한 계산대
+    private FollowTransform followTransform;
+
+    protected virtual void Awake()
+    {
+        followTransform = GetComponent<FollowTransform>();
+    }
 
     public KitchenObjectSO GetKitchenObjectSO()
     {
@@ -18,22 +24,35 @@ public class KitchenObject : NetworkBehaviour
     // 재료의 부모와 위치를 옮김
     public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent)
     {
-        // 계산대에 이미 오브젝트가 위치한 경우
-        if(this.kitchenObjectParent != null)
+        SetKitchenObjectParentServerRpc(kitchenObjectParent.GetNetworkObject());
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SetKitchenObjectParentServerRpc(NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        SetKitchenObjectParentClientRpc(kitchenObjectParentNetworkObjectReference);
+    }
+
+    [ClientRpc]
+    private void SetKitchenObjectParentClientRpc(NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+
+        if (this.kitchenObjectParent != null)
         {
             this.kitchenObjectParent.ClearKitchenObject(); // 오브젝트를 비운다.
         }
         this.kitchenObjectParent = kitchenObjectParent; // 현재 계산대로 수정
 
-        if(kitchenObjectParent.HasKitchenObject()) // 오브젝트를 지웠는데 남아있는 경우
+        if (kitchenObjectParent.HasKitchenObject()) // 오브젝트를 지웠는데 남아있는 경우
         {
             Debug.LogError("이미 물건이 있다.");
         }
 
         kitchenObjectParent.SetKitchenObject(this); // 현재 계산대에 이 오브젝트를 추가
 
-        //transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform(); // 부모 변경
-        //transform.localPosition = Vector3.zero; // 위치 이동
+        followTransform.SetTargetTransform(kitchenObjectParent.GetKitchenObjectFollowTransform());
     }
 
     public IKitchenObjectParent GetKitchenObjectParent()
